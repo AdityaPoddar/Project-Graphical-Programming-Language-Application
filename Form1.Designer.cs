aa12028b9c63ec170caf1413d5d54150f811bf47
Aditya Poddar <adityapoddar.2011@gmail.com>
2022-08-06 22:40:33 +0545

    First Commit Reassessment

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace ASE_Shape_Programming_language
+{
+    partial class ShapeProgrammingLanguage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.figure = new System.Windows.Forms.PictureBox();
+            this.instruction = new System.Windows.Forms.TextBox();
+            this.command = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.figure)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // figure
+            // 
+            this.figure.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.figure.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.figure.Location = new System.Drawing.Point(12, 49);
+            this.figure.Name = "figure";
+            this.figure.Size = new System.Drawing.Size(522, 495);
+            this.figure.TabIndex = 0;
+            this.figure.TabStop = false;
+            this.figure.Paint += new System.Windows.Forms.PaintEventHandler(this.fig_Paint);
+            // 
+            // instruction
+            // 
+            this.instruction.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.instruction.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.instruction.Location = new System.Drawing.Point(554, 49);
+            this.instruction.Multiline = true;
+            this.instruction.Name = "instruction";
+            this.instruction.Size = new System.Drawing.Size(294, 291);
+            this.instruction.TabIndex = 1;
+            // 
+            // command
+            // 
+            this.command.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.command.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.command.Location = new System.Drawing.Point(554, 346);
+            this.command.Name = "command";
+            this.command.Size = new System.Drawing.Size(294, 22);
+            this.command.TabIndex = 2;
+            this.command.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cmd_KeyDown);
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button.Location = new System.Drawing.Point(554, 374);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(294, 31);
+            this.button.TabIndex = 3;
+            this.button.Text = "Run";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.output.ForeColor = System.Drawing.Color.Red;
+            this.output.Location = new System.Drawing.Point(554, 411);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(294, 133);
+            this.output.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(868, 28);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // ShapeProgrammingLanguage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(868, 556);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.command);
+            this.Controls.Add(this.instruction);
+            this.Controls.Add(this.figure);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ShapeProgrammingLanguage";
+            this.Text = "Code Runner";
+            ((System.ComponentModel.ISupportInitialize)(this.figure)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox figure;
+        private System.Windows.Forms.TextBox instruction;
+        private System.Windows.Forms.TextBox command;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace ASE_Shape_Programming_language
+{
+    public partial class ShapeProgrammingLanguage : Form
+    {
+        string cmd;
+        SetValue holdValue;
+        CommandParser cmdParser;
+
+        Bitmap displayBitmap = new Bitmap(492, 495);//bitmap to draw that will be displayed in the picture box
+
+        Bitmap cursorBitmap = new Bitmap(492, 495);// bitmap for displaying the cursour on the picture box
+
+        ArrayList storeFile;// Arraylist to storeFile the file name 
+        public ShapeProgrammingLanguage()
+        {
+            InitializeComponent();
+            holdValue = new SetValue(Graphics.FromImage(displayBitmap), figure);//passing the graphics value to set in the paint class so that it known where to draw
+            cmdParser = new CommandParser(holdValue);//class for handling the drawing, pass the drawing surface to it
+            holdValue.DrawCursor(Graphics.FromImage(cursorBitmap));//providing the cursour image so that it is visible
+            holdValue.SetRunBtn(button);//making the paint class acknowledge the execute button 
+            holdValue.SetTextBox(output);//making the paint class acknowledge the textbox which displays errors 
+        }
+
+        private void cmd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !command.Text.Equals(""))//checking if the key pressed is entere and the text box is not empty
+            {
+                cmd = command.Text.ToLower().Trim();
+
+                if (cmd.Equals("run"))
+                {
+                    button.PerformClick();
+                }
+               else if (cmd.Equals("clear"))
+                {
+                    holdValue.Clear();
+                    Refresh();
+                }
+                else if (cmd.Equals("reset"))
+                {
+                    instruction.Text = "";
+                    holdValue.Reset();
+                    Refresh();
+                }
+                else
+                {
+                    output.Text = "The command given is invalid.";
+                }
+
+                if (cmdParser.errors.Count != 0)//if condition that checks the count is not equal to zero of the arraylist form complie class which is inherited by the commandparser class
+                {
+                    foreach (var item in cmdParser.errors)//running a loop for all the objects coantined inside the arraylist
+                        output.Text += item.ToString();//displaying the error message in the consodle textbox
+                }
+                Refresh();  //signify that display needs updating
+
+                cmdParser.ClearErrors();//calling the ClearError method of the complie class which is inherited by the commandparser class to empty the arraylist from its values
+
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (command.Text.ToLower().Trim().Equals("run"))
+            {
+                output.Text = " ";
+                string instructions = instruction.Text;
+                cmdParser.Validate(instructions);
+            }
+            else if (command.Text.ToLower().Trim().Equals("clear"))
+            {
+                holdValue.Clear();
+                Refresh();
+            }
+            else if (command.Text.ToLower().Trim().Equals("reset"))
+            {
+                instruction.Text = "";
+                holdValue.Reset();
+                Refresh();
+            }
+            else
+            {
+                output.Text = "The command given is invalid.";
+            }
+
+
+            if (cmdParser.errors.Count != 0)//if condition that checks the count is not equal to zero of the arraylist form complie class which is inherited by the commandparser class
+            {
+                foreach (var item in cmdParser.errors)//running a loop for all the objects coantined inside the arraylist
+                    output.Text += item.ToString();//displaying the error message in the consodle textbox
+            }
+            Refresh();  //signify that display needs updating
+
+            cmdParser.ClearErrors();//calling the ClearError method of the complie class which is inherited by the commandparser class to empty the arraylist from its values
+
+        }
+
+        private void fig_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; //get graphics context where the shapes are to be created
+
+            holdValue.DrawCursor(Graphics.FromImage(cursorBitmap));//creating the red dot that is seen on the picturebox
+
+            g.DrawImageUnscaled(displayBitmap, 0, 0);
+            g.DrawImageUnscaled(cursorBitmap, 0, 0);
+            cursorBitmap.MakeTransparent();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            instruction.Text = "";//restting the vlaue of cmdtext to be empty
+
+            storeFile = new FileLoadAndWrite().LoadFile();//assigning the file value to the arraylist
+
+            foreach (string s in storeFile) //running the loop unitl the lenght of the arraylist 
+            {
+                instruction.Text += s +"\n"; //print text present inside the file on cmdText textbox
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new FileLoadAndWrite().WriteFile(instruction.Text);//creating a new file with the name that the user provided where all the text present inside the cmdText box is present
+        }
+    }
+}
