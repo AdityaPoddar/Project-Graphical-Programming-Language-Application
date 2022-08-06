347ac300dca42f49c70aaa554e984e4031c92bec
Aditya Poddar <adityapoddar.2011@gmail.com>
2022-08-06 22:45:15 +0545

    Neccessary files

---
+++ ShapeFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Shape_Programming_language
+{
+    class ShapeFactory
+    {
+        public Shape getShape(String shapeType)
+        {
+            shapeType = shapeType.ToUpper().Trim();
+
+
+            if (shapeType.Equals("CIRCLE"))
+            {
+                return new Circle();
+
+            }
+            if (shapeType.Equals("RECTANGLE"))
+            {
+                return new Rectangle();
+
+            }
+            else if (shapeType.Equals("TRIANGLE"))
+            {
+                return new Triangle();
+            }
+            else
+            {
+                return null;
+
+            }
+
+
+        }
+
+    }
+}
